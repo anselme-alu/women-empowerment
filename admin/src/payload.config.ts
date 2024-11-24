a6c8e2715a2a56a79c528015213f74f19fffe61b
Anselme Irumva <andesanselme@gmail.com>
2024-11-24 17:03:41 +0200

    fix: logo and add storage

--- admin/src/payload.config.ts
+++ admin/src/payload.config.ts
@@ -17,6 +17,8 @@ import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
 
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
+
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -68,6 +70,15 @@ export default buildConfig({
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: {
+          prefix: "women-empowerment"
+        }
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN!,
+    }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
